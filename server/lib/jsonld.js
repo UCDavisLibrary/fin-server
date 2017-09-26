8f82ac78ab7dbbe4a0a8f9dc2b8692406a3a003b
Jusitn Merz <jrmerz@gmail.com>
2017-09-26 09:36:35 -0700

    fix for prod build

--- server/lib/jsonld.js
+++ server/lib/jsonld.js
@@ -4,20 +4,18 @@ var config = require('../config');
 
 jsonld.compact = util.promisify(jsonld.compact);
 
-var context = config.jsonld.compactContext;
-var keep = config.jsonld.keep;
-
 class JsonldUtils {
 
   async compact(doc) {
-    return await jsonld.compact(doc, context);
+    return await jsonld.compact(doc, config.jsonld.compactContext);
   }
 
   async clean(doc) {
     doc = await this.compact(doc);
 
     for( var key in doc ) {
-      if( !context[key] && keep.indexOf(key) === -1 ) {
+      if( !config.jsonld.compactContext[key] && 
+           config.jsonld.keep.indexOf(key) === -1 ) {
         delete doc[key];
       }
     }
