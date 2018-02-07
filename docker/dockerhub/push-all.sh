50889626090a61dda62e3e78d39b3211fcfac279
Justin Merz <jrmerz@gmail.com>
2018-02-07 13:36:22 -0800

    adding dockerhub builds

--- docker/dockerhub/push-all.sh
+++ docker/dockerhub/push-all.sh
@@ -5,8 +5,10 @@ cd $DIR
 
 ./fin-server.sh
 ./fin-fcrepo.sh
+./fin-ucd-lib-client-service.sh
 ./fin-trusted-proxy.sh
 ./fin-cas-service.sh
+./fin-basic-auth-service.sh
 ./fin-essync-service.sh
 ./fin-loris-service.sh
 ./fin-serialization-service.sh
