4095e0c3e24155fc32a01a315e452cd33e1c4aae
Justin Merz <jrmerz@gmail.com>
2018-05-17 10:08:47 -0700

    fix for pinterest image, adding some share btn title/description, issue #54

--- services/ucd-lib-client/client/public/elements/utils/app-share-btn.js
+++ services/ucd-lib-client/client/public/elements/utils/app-share-btn.js
@@ -2,17 +2,18 @@ import {PolymerElement} from "@polymer/polymer/polymer-element"
 import template from "./app-share-btn.html"
 
 import AppStateInterface from "../interfaces/AppStateInterface"
+import MediaInterface from "../interfaces/MediaInterface"
 
 const BASE_SHARE_LINKS = {
-  facebook : 'https://www.facebook.com/sharer/sharer.php?u=',
-  google : 'https://plus.google.com/share?url=',
-  twitter : 'https://twitter.com/home?status=',
+  facebook : 'https://www.facebook.com/sharer/sharer.php',
+  google : 'https://plus.google.com/share',
+  twitter : 'https://twitter.com/home',
   // pinterest can also add ?media and ?description
-  pinterest : 'https://pinterest.com/pin/create/button/?url='
+  pinterest : 'https://pinterest.com/pin/create/button/'
 }
 
 export default class AppShareBtn extends Mixin(PolymerElement)
-  .with(EventInterface, AppStateInterface) {
+  .with(EventInterface, AppStateInterface, MediaInterface) {
 
   static get template() {
     let tag = document.createElement('template');
@@ -27,6 +28,11 @@ export default class AppShareBtn extends Mixin(PolymerElement)
         value : false
       }
     }
+  }
+
+  constructor() {
+    super();
+    this.active = true;
   }
 
   ready() {
@@ -78,6 +84,16 @@ export default class AppShareBtn extends Mixin(PolymerElement)
   }
 
   /**
+   * @method _onSelectedRecordUpdate
+   * @description from AppStateInterface, called when a record is selected
+   * 
+   * @param {Object} record selected record
+   */
+  _onSelectedRecordUpdate(record) {
+    this.record = record;
+  }
+
+  /**
    * @method _onSocialIconClick
    * @description bound to social icon buttons.  Called when one os clicked
    * 
@@ -85,10 +101,36 @@ export default class AppShareBtn extends Mixin(PolymerElement)
    */
   _onSocialIconClick(e) {
     if( e.type === 'keyup' && e.which !== 13 ) return;
-    debugger;
     let id = e.currentTarget.id;
-    let url = BASE_SHARE_LINKS[id]+encodeURIComponent(window.location.href);
+
+    let url = BASE_SHARE_LINKS[id];
+    let qso = {};
+
+    if( id === 'pinterest' ) {  
+      let path = this._getImgPath(this.record);
+      if( path ) {
+        qso.media = window.location.protocol+'//'+window.location.host+this._getImgUrl(path);
+      }
+      qso.description = this.record.name;
+      qso.url = window.location.href;
+    } else if ( id === 'facebook' ) {
+      qso.u = window.location.href;
+    } else if ( id === 'google' ) {
+      qso.url = window.location.href;
+    } else if( id === 'twitter' ) {
+      qso.status = this.record.name+' - '+window.location.href;
+    }
+
+    url += this._createQs(qso);
     window.open(url, '_blank', 'height=400,width=500');
+  }
+
+  _createQs(qso) {
+    let query = [];
+    for( let key in qso ) {
+      query.push(key+'='+encodeURIComponent(qso[key]));
+    }
+    return '?'+query.join('&');
   }
 
   /**
