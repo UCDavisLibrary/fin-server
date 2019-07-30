42e8e88804e5808f9a85709fb1c07bb5278c979a
Leigh Dragoon <ladragoon@ucdavis.edu>
2019-07-30 15:25:56 -0700

    Fixed problem with ucd-dams-local-dev/docker-compose.yml

--- services/ucd-lib-client/client/public/elements/pages/record/viewer/app-video-viewer.js
+++ services/ucd-lib-client/client/public/elements/pages/record/viewer/app-video-viewer.js
@@ -68,8 +68,7 @@ export default class AppVideoViewer extends Mixin(PolymerElement)
       sources: [
         {
           src: this.url,
-          type: 'video/mp4'
-          //type: media['encodingFormat']
+          type: media['encodingFormat']
         }
       ],
       poster: this.poster,
@@ -78,116 +77,7 @@ export default class AppVideoViewer extends Mixin(PolymerElement)
       iconUrl: '../../../../images/sprite/',
       
       // Blank video (used to prevent errors on source change)
-      blankVideo: '../../../../node_modules/plyr/dist/blank.mp4',
-      
-      // Selectors  
-      // Change these to match your template if using custom HTML
-      selectors: {
-        editable: 'input, textarea, select, [contenteditable]',
-        container: '.plyr',
-        controls: {
-          container: null,
-          wrapper: '.plyr__controls',
-        },
-        labels: '[data-plyr]',
-        buttons: {
-          play: '[data-plyr="play"]',
-          pause: '[data-plyr="pause"]',
-          restart: '[data-plyr="restart"]',
-          rewind: '[data-plyr="rewind"]',
-          fastForward: '[data-plyr="fast-forward"]',
-          mute: '[data-plyr="mute"]',
-          captions: '[data-plyr="captions"]',
-          download: '[data-plyr="download"]',
-          fullscreen: '[data-plyr="fullscreen"]',
-          pip: '[data-plyr="pip"]',
-          airplay: '[data-plyr="airplay"]',
-          settings: '[data-plyr="settings"]',
-          loop: '[data-plyr="loop"]',
-        },
-        inputs: {
-          seek: '[data-plyr="seek"]',
-          volume: '[data-plyr="volume"]',
-          speed: '[data-plyr="speed"]',
-          language: '[data-plyr="language"]',
-          quality: '[data-plyr="quality"]',
-        },
-        display: {
-          currentTime: '.plyr__time--current',
-          duration: '.plyr__time--duration',
-          buffer: '.plyr__progress__buffer',
-          loop: '.plyr__progress__loop', // Used later
-          volume: '.plyr__volume--display',
-        },
-        progress: '.plyr__progress',
-        captions: '.plyr__captions',
-        caption: '.plyr__caption',
-        menu: {
-          quality: '.js-plyr__menu__list--quality',
-        },
-      },
-
-      // Class hooks added to the player in different states
-      classNames: {
-        type: 'plyr--{0}',
-        provider: 'plyr--{0}',
-        video: 'plyr__video-wrapper',
-        embed: 'plyr__video-embed',
-        embedContainer: 'plyr__video-embed__container',
-        poster: 'plyr__poster',
-        posterEnabled: 'plyr__poster-enabled',
-        ads: 'plyr__ads',
-        control: 'plyr__control',
-        controlPressed: 'plyr__control--pressed',
-        playing: 'plyr--playing',
-        paused: 'plyr--paused',
-        stopped: 'plyr--stopped',
-        loading: 'plyr--loading',
-        hover: 'plyr--hover',
-        tooltip: 'plyr__tooltip',
-        cues: 'plyr__cues',
-        hidden: 'plyr__sr-only',
-        hideControls: 'plyr--hide-controls',
-        isIos: 'plyr--is-ios',
-        isTouch: 'plyr--is-touch',
-        uiSupported: 'plyr--full-ui',
-        noTransition: 'plyr--no-transition',
-        display: {
-          time: 'plyr__time',
-        },
-        menu: {
-          value: 'plyr__menu__value',
-          badge: 'plyr__badge',
-          open: 'plyr--menu-open',
-        },
-        captions: {
-          enabled: 'plyr--captions-enabled',
-          active: 'plyr--captions-active',
-        },
-        fullscreen: {
-          enabled: 'plyr--fullscreen-enabled',
-          fallback: 'plyr--fullscreen-fallback',
-        },
-        pip: {
-          supported: 'plyr--pip-supported',
-          active: 'plyr--pip-active',
-        },
-        airplay: {
-          supported: 'plyr--airplay-supported',
-          active: 'plyr--airplay-active',
-        },
-        tabFocus: 'plyr__tab-focus',
-        previewThumbnails: {
-          // Tooltip thumbs
-          thumbContainer: 'plyr__preview-thumb',
-          thumbContainerShown: 'plyr__preview-thumb--is-shown',
-          imageContainer: 'plyr__preview-thumb__image-container',
-          timeContainer: 'plyr__preview-thumb__time-container',
-          // Scrubbing
-          scrubbingContainer: 'plyr__preview-scrubbing',
-          scrubbingContainerShown: 'plyr__preview-scrubbing--is-shown',
-        }
-      }
+      blankVideo: '../../../../node_modules/plyr/dist/blank.mp4'
     }
   }
 }
