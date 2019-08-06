f4e61f1ee4d07cdf033c6b47b42f20166f032daf
Leigh Dragoon <ladragoo@campus-038-203.ucdavis.edu>
2019-08-06 15:22:08 -0700

    commented out some console logging

--- services/ucd-lib-client/client/public/elements/pages/record/app-media-download.js
+++ services/ucd-lib-client/client/public/elements/pages/record/app-media-download.js
@@ -141,7 +141,7 @@ export default class AppMediaDownload extends PolymerElement {
   
       const i = Math.floor(Math.log(bytes) / Math.log(k));
   
-      return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
+      return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + '\xa0' + sizes[i];
     }
 
     this.fileSize = formatBytes(options.size);
