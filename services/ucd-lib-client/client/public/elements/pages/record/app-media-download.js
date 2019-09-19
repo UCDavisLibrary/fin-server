d7bcc54fe20d55af8ba5c5654307a448a22ce08e
Leigh Dragoon <ladragoo@campus-025-182.ucdavis.edu>
2019-09-19 09:27:44 -0700

    Almost done fixing app-media-download

--- services/ucd-lib-client/client/public/elements/pages/record/app-media-download.js
+++ services/ucd-lib-client/client/public/elements/pages/record/app-media-download.js
@@ -1,6 +1,9 @@
 import {PolymerElement} from "@polymer/polymer/polymer-element"
 import template from "./app-media-download.html"
 
+import CollectionInterface from "../../interfaces/CollectionInterface"
+import MediaInterface from "../../interfaces/MediaInterface"
+
 import MediaModel from "../../../lib/models/MediaModel"
 
 import config from "../../../lib/config"
@@ -8,7 +11,7 @@ import utils from "../../../lib/utils"
 import bytes from "bytes"
 
 // Full Resolution - Default
-const SIZES = [
+const IMG_SIZES = [
   {
     title : 'Small',
     label : 'S',
@@ -31,9 +34,10 @@ const SIZES = [
   }
 ]
 
-const FORMATS = ['png', 'jpg', 'webp'];
+const IMG_FORMATS = ['png', 'jpg', 'webp'];
 
-export default class AppMediaDownload extends PolymerElement {
+export default class AppMediaDownload extends Mixin(PolymerElement)
+      .with(EventInterface, CollectionInterface, MediaInterface) {
 
   static get template() {
     let tag = document.createElement('template');
@@ -51,10 +55,6 @@ export default class AppMediaDownload extends PolymerElement {
         type : String,
         value : ''
       },
-      resolutionTitle : {
-        type : String,
-        value : 'Full Resolution'
-      },
       isVideo: {
         type: Boolean,
         value: false
@@ -67,27 +67,27 @@ export default class AppMediaDownload extends PolymerElement {
         type : String,
         value : ''
       },
-      sizes : {
+      imageSizes : {
         type : Array,
         value : () => []
+      },
+      sources: {
+        type: Array,
+        value: () => []
       },
       formats : {
         type : Array,
         value : () => []
       },
-      defaultImage : {
+      defaultImage: {
         type : Boolean,
         value : true
-      },
-      hasMultipleImages : {
-        type : Boolean,
-        value : false
       },
       hasMultipleSources: {
         type: Boolean,
         value: false
       },
-      multipleImagesSelected : {
+      multipleSourcesSelected: {
         type : Boolean,
         value : false
       }
@@ -97,6 +97,7 @@ export default class AppMediaDownload extends PolymerElement {
   constructor() {
     super();
     this.active = true;
+    this._injectModel('AppStateModel');
   }
 
   /**
@@ -110,12 +111,15 @@ export default class AppMediaDownload extends PolymerElement {
    * @param {String} options.url fedora image url
    */
   render(options) {
-    if (options === undefined) {
-      return;
-    }
+    if (options === undefined) return;
+    this.options = options.fileFormat;
+    this.originalFormat = options.fileFormat.replace(/.*\//, '').toLowerCase();
+    this.size = bytes(options.size);
 
-    this.options = options;
-    this.sizes = SIZES.map((format, index) => {
+    this.$.format.value = this.originalFormat;
+    this.defaultImage = true;
+
+    this.imageSizes = IMG_SIZES.map((format, index) => {
       return {
         title : format.title,
         label : format.label,
@@ -125,34 +129,99 @@ export default class AppMediaDownload extends PolymerElement {
       }
     });
 
-    this.hasMultipleImages = false;
-    this.multipleImagesSelected = false;
-    this.size = bytes(options.size);
-    this.originalFormat = options.fileFormat.replace(/.*\//, '').toLowerCase(); 
-    this.$.format.value = this.originalFormat;
-    this.defaultImage = true;
-
-    this._renderFormats();    
+    this._renderImgFormats();
   }
 
   setRootRecord(record) {
     if( this.rootRecord === record ) return;
     this.rootRecord = record;
 
-    if (this.rootRecord.media.video) {
-      this.isVideo = true;
-      let video = utils.formatVideo(this.rootRecord.media.video[0]);
+    this.multipleSourcesSelected = false;
+    this.selectedSize = IMG_SIZES.length - 1;
+  }
 
-      if (video.sources && video.sources.length > 0) {
-        video.sources.forEach(element => {
-          element.type = ((element.type !== undefined) ? element.type.replace(/.*\//, '') : '');
-          element.fileSize = bytes(element.fileSize);
-          element.src  = config.fcrepoBasePath+element.src;
-          element.label = element.type + (element.fileSize ? ' (' + element.fileSize + ') ' : '');
-        });
-        this.downloadOptions = video.sources;
-        this.href = this.downloadOptions[0].src;
+  _onSelectedRecordMediaUpdate(record) {
+    if (utils.getType(record) === 'video') {
+      this.isVideo = true;
+      this.sources = this._getVideoSources(record);
+      this.href = this.sources[0].src;
+    } else if (utils.getType(record) === 'audio') {
+      this.isVideo = true;
+      this.sources = this._getAudioSources(record);
+    } else {
+      this.isVideo = false;
+      this.sources = this._getImageSources(record);
+      if (this.sources.length > 1){
+        this.imagelist = this.sources;
       };
+
+      this.imageSizes = IMG_SIZES.map((format, index) => {
+        return {
+          title : format.title,
+          label : format.label,
+          width: Math.floor(this.sources[0].image.width * format.ratio),
+          height: Math.floor(this.sources[0].image.height * format.ratio),
+          selected : (this.selectedSize === index)
+        }
+      });
+
+      this._renderImgFormats();
+    }
+
+    this.downloadOptions = this.sources;
+
+    if (this.downloadOptions.length === 1) {
+      this.$.videoDownloadOptions.disabled  = true;
+      this.$.videoDownloadOptions.classList.add("plainText");
+    } else {
+      this.$.videoDownloadOptions.disabled = false;
+      this.$.videoDownloadOptions.classList.remove("plainText");
+    }
+
+    if (this.downloadOptions && this.downloadOptions.length > 1) this.hasMultipleSources = true;
+    else this.hasMultipleSources = false;
+  }
+
+  _getImageSources(imageRecord) {
+    let sources = [], image = imageRecord;
+
+    if (utils.getType(image) === 'imageList') {
+      sources = image.hasPart;
+    } else {
+      sources.push(image);
+    };
+
+    return sources;
+  }
+
+  _getAudioSources(audioRecord) {
+    let sources = [], obj = {}, audio = audioRecord;
+    
+    obj = {
+      src: config.fcrepoBasePath + audio['@id'],
+      type: audio.encodingFormat.split('/').pop(),
+      label: audio.encodingFormat.split('/').pop() + (bytes(audio.fileSize) ? ' (' + bytes(audio.fileSize) + ') ' : '')
+    };
+
+    sources.push(obj);
+
+    return sources;
+  }
+
+  _getVideoSources(videoRecord) {
+    let sources = [];
+
+    let video = utils.formatVideo(videoRecord);
+
+    if (video.sources && video.sources.length > 0) {
+      video.sources.forEach(element => {
+        element.type = ((element.type !== undefined) ? element.type.replace(/.*\//, '') : '');
+        element.fileSize = bytes(element.fileSize);
+        element.src  = config.fcrepoBasePath+element.src;
+        element.label = element.type + (element.fileSize ? ' (' + element.fileSize + ') ' : '');
+      });
+
+      sources = video.sources;
 
       if (video.transcripts && video.transcripts.length > 0) {
         video.transcripts.forEach(transcript => {
@@ -161,30 +230,13 @@ export default class AppMediaDownload extends PolymerElement {
             type: transcript.src.split('.').pop(),
             label: transcript.src.split('.').pop() + ' (transcript only)'
           }
-          
-          this.downloadOptions.push(obj);
+
+          sources.push(obj);
         });
       }
-
-      if (this.downloadOptions && this.downloadOptions.length > 1) {
-        this.hasMultipleSources = true;
-      } else {
-        this.$.videoDownloadOptions.disabled  = true;
-        this.$.videoDownloadOptions.className = "plainText";
-      }
     }
 
-    if ( this.downloadOptions && this.downloadOptions.length > 1 ) {
-      this.hasMultipleSources = true;
-    }
-
-    if ( this.rootRecord.media.imageList ) {
-      this.hasMultipleImages = (this.rootRecord.media.imageList[0].hasPart.length > 0);
-      this.imagelist = this.rootRecord.media.imageList[0].hasPart;
-    }
-  
-    this.multipleImagesSelected = false;
-    this.selectedSize = SIZES.length - 1;
+    return sources;
   }
 
   _onChangeVideoDownloadOptions(e) {
@@ -193,24 +245,23 @@ export default class AppMediaDownload extends PolymerElement {
   }
 
   /**
-   * @method _renderFormats
+   * @method _renderImgFormats
    * @private
-   * @description render formats select element based of static format 
+   * @description render image formats select element based of static format 
    * list and additional native format if not in list and size is at
    * full resolution.
    */
-  _renderFormats() {
+  _renderImgFormats() {
     let formats;
 
-    formats = FORMATS.slice(0);
+    formats = IMG_FORMATS.slice(0);
     if( this.originalFormat &&
-        this.selectedSize === SIZES.length - 1 &&
+        this.selectedSize === IMG_SIZES.length - 1 &&
         formats.indexOf(this.originalFormat) === -1 ) {
       formats.unshift(this.originalFormat);
     }
 
     this.formats = formats;
-
     this.$.format.innerHTML = '';
     this.formats.forEach(format => {
       let option = document.createElement('option');
@@ -223,7 +274,7 @@ export default class AppMediaDownload extends PolymerElement {
       
       this.$.format.appendChild(option);
     });
-
+    
     this._renderDownloadHref();
   }
 
@@ -232,11 +283,11 @@ export default class AppMediaDownload extends PolymerElement {
    * @private
    * @description called when user selects a size button.  Toggle over buttons
    * to off state and updates formats based on current size.
-   */
+  */
   _onSizeChange(e) {
     let selected = e.currentTarget.value;
 
-    this.sizes = this.sizes.map((size, index) => {
+    this.imageSizes = this.imageSizes.map((size, index) => {
       if( selected === size.label ) {
         size.selected = true;
         this.selectedSize = index;
@@ -247,9 +298,7 @@ export default class AppMediaDownload extends PolymerElement {
       return Object.assign({}, size);
     });
     
-    // this.resolutionTitle = this.sizes[this.selectedSize].title;
-
-    this._renderFormats();
+    this._renderImgFormats();
   }
 
   /**
@@ -273,34 +322,33 @@ export default class AppMediaDownload extends PolymerElement {
    * tag, we need to make sure then options have rendered.  So we call 
    * requestAnimationFrame to pause to allow time for generation.  This
    * is probably NOT the best way to do things.
-   */
-  _renderDownloadHref() {    
+  */
+  _renderDownloadHref() {
     requestAnimationFrame(() => {
-      // this.resolution = this.sizes[this.selectedSize].size.join(' x ')+' px';
-
+      // this.resolution = this.imageSizes[this.selectedSize].size.join(' x ')+' px';
+      
       if( !this.selectedFormat || this.formats.indexOf(this.selectedFormat) === -1 ) {
         this.selectedFormat = this.formats[0].replace(/ .*/, '');
         this.$.format.value = this.formats[0];
       }
-    
+
       if( this.$.format.value !== this.selectedFormat ) {
         this.$.format.value = this.selectedFormat;
       }
 
       this._setTarPaths();
 
-      if( this.selectedFormat === this.originalFormat && 
-        this.selectedSize === SIZES.length -1 ) {
-          this.defaultImage = true;
-          return this.href = this.options.url;
+      if( this.selectedFormat === this.originalFormat && this.selectedSize === IMG_SIZES.length -1 ) {
+        this.defaultImage = true;
+        return this.href = this.options.url;
       }
 
       this.defaultImage = false;
 
-      let size = this.sizes[this.selectedSize];
-      size = size.width+','+size.height;
-
-      this.href = this.options.url + `/svc:iiif/full/${size}/0/default.${this.selectedFormat}`;
+      let size = this.imageSizes[this.selectedSize];
+      size = size.width + ',' + size.height;
+      
+      //this.href = this.options.url + `/svc:iiif/full/${size}/0/default.${this.selectedFormat}`;
     });
   }
 
@@ -322,27 +370,24 @@ export default class AppMediaDownload extends PolymerElement {
    * @description bound to radio buttons click event
    */
   _toggleMultipleDownload() {
-    this.multipleImagesSelected = this.$.fullset.checked ? true : false;
+    this.multipleSourcesSelected = this.$.fullset.checked ? true : false;
   }
 
   _setTarPaths() {
-    if( !SIZES[this.selectedSize] ) return;
+    if( !IMG_SIZES[this.selectedSize] ) return;
+    let origin = false, urls = {};
 
-    let origin = false;
-
-    if( this.selectedFormat === this.originalFormat && this.selectedSize === SIZES.length -1 ) {
+    if( this.selectedFormat === this.originalFormat && this.selectedSize === IMG_SIZES.length -1 ) {
       origin = true;
     }
 
-    let urls = {};
-
-    if ( this.hasMultipleImages ) {
+    if ( this.hasMultipleSources ) {
       this.imagelist.forEach(item => {
         let name = item.filename || item.name;
         if( origin ) {
           urls[name] = item['@id'];
         } else {
-          let s = SIZES[this.selectedSize];
+          let s = IMG_SIZES[this.selectedSize];
           name = name.replace(/\.[a-z]*$/, `_${s.label}_.${this.selectedFormat}`);
           let w = Math.floor(item.image.width * s.ratio);
           let h = Math.floor(item.image.height * s.ratio);
@@ -350,9 +395,9 @@ export default class AppMediaDownload extends PolymerElement {
         }
       });
     }
-      
-    this.tarName = this.rootRecord.name.replace(/[^a-zA-Z0-9]/g, '');
+
     this.$.tarPaths.value = JSON.stringify(urls);
+    this.tarName = this.rootRecord.name.replace(/[^a-zA-Z0-9]/g, '');
   }
 
   /**
