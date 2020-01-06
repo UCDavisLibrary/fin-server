2abdcf508f38f09a0fb2a86cc1807405e34c2500
Leigh Dragoon <ladragoo@campus-004-150.ucdavis.edu>
2020-01-06 13:56:23 -0800

    Removed collection events from app-footer.js

--- services/ucd-lib-client/client/public/elements/app-footer.js
+++ services/ucd-lib-client/client/public/elements/app-footer.js
@@ -1,13 +1,11 @@
 import {PolymerElement} from "@polymer/polymer/polymer-element"
 import AppStateInterface from "./interfaces/AppStateInterface"
-import CollectionInterface from "./interfaces/CollectionInterface"
-import RecordInterface from "./interfaces/RecordInterface"
 import "./auth/app-auth-footer"
 
 import template from "./app-footer.html"
 
 class AppFooter extends Mixin(PolymerElement)
-      .with(EventInterface, AppStateInterface, CollectionInterface, RecordInterface) {
+      .with(EventInterface, AppStateInterface) {
   
   static get template() {
     let tag = document.createElement('template');
@@ -16,42 +14,13 @@ class AppFooter extends Mixin(PolymerElement)
   }
 
   static get properties() {
-    return {
-      // JSON payload of collection overview
-      collections : {
-        type : Array,
-        value : () => []
-      }
-    }
+    return { }
   }
 
   constructor() {
     super();
     this.active = true;
-  }
-
-  /**
-   * @method _onCollectionOverviewUpdate
-   * @description from CollectionInterface, called when collection overview query completes
-   */
-  _onCollectionOverviewUpdate(e) {
-    if( e.state !== 'loaded' ) return;
-    this.collections = e.payload.map(item => ({
-      id: item['@id'],
-      name : item.name
-    }));
-  }
-
-  /**
-   * @method _onBrowseCollection
-   * @description from click event, called when collection image is clicked
-   * 
-   * @param {Object} e HTML click event 
-   */
-  _onBrowseCollection(e) {
-    let id = e.currentTarget.getAttribute('data-id');
-    this._setWindowLocation(id);
-  }
+  }  
 }
 
 customElements.define('app-footer', AppFooter);
